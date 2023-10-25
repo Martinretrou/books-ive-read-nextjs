5c91aa6ac8ad1ba08ad4e70ccd9a48f967a6507f
Martinretrou <martinretrou@gmail.com>
2023-10-25 19:58:25 +0200

    :bug: Fix current year in hero component

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -7,29 +7,33 @@ export type FilterType = {
   value: string;
 };
 
-const Hero = () => (
-  <header className={classNames(`grid-1-cols`, styles.hero)}>
-    <div className={styles.heroContent}>
-      <div className={styles.heroTitle}>
-        <h1>
-          Books I've read <br /> <mark>2016 - 2022</mark>
-        </h1>
+const Hero = () => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <header className={classNames(`grid-1-cols`, styles.hero)}>
+      <div className={styles.heroContent}>
+        <div className={styles.heroTitle}>
+          <h1>
+            Books I've read <br /> <mark>2016 - {currentYear}</mark>
+          </h1>
+        </div>
+        <div className={styles.heroDescription}>
+          <p>
+            Here's a list of the books I've read since 2016. I've started
+            recording my readings since I've fallen in the science-fiction
+            rabbit hole.
+            <br />
+            <br />
+            The list is mostly composed of science-fiction books ranging from
+            some classics (Isaac Asimov, Frank Herbert, Dan Simmons, etc.) and
+            some recent authors (Jeff Vandermeer, Emma Newman, and more).
+            There's also some thriller, science, and others types of books.
+          </p>
+        </div>
       </div>
-      <div className={styles.heroDescription}>
-        <p>
-          Here's a list of the books I've read since 2016. I've started
-          recording my readings since I've fallen in the science-fiction rabbit
-          hole.
-          <br />
-          <br />
-          The list is mostly composed of science-fiction books ranging from some
-          classics (Isaac Asimov, Frank Herbert, Dan Simmons, etc.) and some
-          recent authors (Jeff Vandermeer, Emma Newman, and more). There's also
-          some thriller, science, and others types of books.
-        </p>
-      </div>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 
 export default Hero;
